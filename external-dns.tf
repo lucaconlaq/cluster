1eceffcd88d7c33dd185100703762a7418f5719e
Luca <luca.querella@gmail.com>
2025-05-02 12:44:28 +0200

    Use workload identity for external dns

--- external-dns.tf
+++ external-dns.tf
@@ -10,8 +10,10 @@ resource "google_project_iam_member" "externaldns_dns_admin" {
   member  = "serviceAccount:${google_service_account.externaldns.email}"
 }
 
-resource "google_service_account_key" "externaldns" {
+resource "google_service_account_iam_member" "externaldns_workload_identity" {
   service_account_id = google_service_account.externaldns.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[external-dns/external-dns]"
 }
 
 resource "kubernetes_namespace" "external_dns" {
@@ -20,20 +22,23 @@ resource "kubernetes_namespace" "external_dns" {
   }
 }
 
-resource "kubernetes_secret" "externaldns_credentials" {
-  depends_on = [google_service_account_key.externaldns]
+resource "kubernetes_service_account" "externaldns" {
   metadata {
-    name = "external-dns"
+    name      = "external-dns"
     namespace = kubernetes_namespace.external_dns.metadata[0].name
-  }
-
-  data = {
-    "credentials.json" = base64decode(google_service_account_key.externaldns.private_key)
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.externaldns.email
+    }
   }
 }
 
 resource "helm_release" "external_dns" {
-  depends_on = [kubernetes_namespace.nginx_ingress, kubernetes_secret.externaldns_credentials]
+  depends_on = [
+    kubernetes_namespace.nginx_ingress,
+    kubernetes_namespace.external_dns,
+    kubernetes_service_account.externaldns
+  ]
+
   name       = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "external-dns"
@@ -48,16 +53,6 @@ resource "helm_release" "external_dns" {
   set {
     name  = "google.project"
     value = var.project_id
-  }
-
-  set {
-    name  = "google.serviceAccountSecret"
-    value = kubernetes_secret.externaldns_credentials.metadata.0.name
-  }
-
-  set {
-    name  = "google.serviceAccountSecretKey"
-    value = keys(kubernetes_secret.externaldns_credentials.data).0
   }
 
   set {
@@ -108,5 +103,15 @@ resource "helm_release" "external_dns" {
   set {
     name  = "sources[1]"
     value = "ingress"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
   }
 }
